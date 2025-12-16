73800cae2ecadd51ff870543494fe7212e632e78
sethupathi J P <23becy052@kahedu.edu.in>
2025-12-16 09:52:44 +0530

    Add files via upload

---
+++ all (1).cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=5;i>=0;i--)
+    cout<<i<<"\n ";
+}
---
+++ all (2).cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<=10;i++)
+    if(i%2==0)
+    cout<<i<<"\n";
+}
