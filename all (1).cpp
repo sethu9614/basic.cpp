7525174cc3bc6bb1b02a90db9d2f6de5c619503f
sethupathi J P <23becy052@kahedu.edu.in>
2025-12-16 10:17:44 +0530

    Add files via upload

--- all (1).cpp
+++ all (1).cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=5;i>=0;i--)
-    cout<<i<<"\n ";
+    while(1){
+        int val;
+        cin>>val;
+        if(val==-1){
+            break;
+        }
+    }
 }
--- all.cpp
+++ all.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=1;i<=5;i++)
-    cout<<"Hello\n";
+   int i=1;
+while(i<=5){
+    cout<<i;
+    i++;
+}
 }
